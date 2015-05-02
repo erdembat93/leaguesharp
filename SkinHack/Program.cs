2d050646f6d71422696e28d11c22b3ec8927ebcb
trees-software <treesbot@gmail.com>
2015-05-02 17:43:19 -0400

    skinhack

---
+++ SkinHack/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Web.Script.Serialization;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace SkinHack
+{
+    internal class Program
+    {
+        public static Menu Config;
+        public static String DataDragonBase = "http://ddragon.leagueoflegends.com/";
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            new Thread(
+                () =>
+                {
+                    Config = new Menu("SkinHax", "SkinHax", true);
+                    var versionJson = new WebClient().DownloadString(DataDragonBase + "realms/na.json");
+                    var gameVersion =
+                        (String)
+                            ((Dictionary<String, Object>)
+                                new JavaScriptSerializer().Deserialize<Dictionary<String, Object>>(versionJson)["n"])[
+                                    "champion"];
+                    foreach (var hero in ObjectManager.Get<Obj_AI_Hero>())
+                    {
+                        var champJson =
+                            new WebClient().DownloadString(
+                                DataDragonBase + "cdn/" + gameVersion + "/data/en_US/champion/" + hero.ChampionName +
+                                ".json");
+                        var skins =
+                            (ArrayList)
+                                ((Dictionary<String, Object>)
+                                    ((Dictionary<String, Object>)
+                                        new JavaScriptSerializer().Deserialize<Dictionary<String, Object>>(champJson)[
+                                            "data"])[hero.ChampionName])["skins"];
+                        var champMenu = new Menu(hero.ChampionName, hero.ChampionName, false);
+                        foreach (Dictionary<string, object> skin in skins)
+                        {
+                            var skinName = skin["name"].ToString();
+                            if (skinName.Equals("default"))
+                            {
+                                skinName = hero.ChampionName;
+                            }
+                            var changeSkin = champMenu.AddItem(new MenuItem(skinName, skinName).SetValue(false));
+                            changeSkin.ValueChanged += (s, e) =>
+                            {
+                                if (e.GetNewValue<bool>())
+                                {
+                                    champMenu.Items.ForEach(
+                                        p =>
+                                        {
+                                            if (p.GetValue<bool>() && p.Name != skinName)
+                                            {
+                                                p.SetValue(false);
+                                            }
+                                        });
+                                    hero.SetSkin(hero.ChampionName, (int) skin["num"]);
+                                }
+                            };
+                        }
+                        Config.AddSubMenu(champMenu);
+                    }
+                    Config.AddToMainMenu();
+                }).Start();
+
+            Game.OnInput += Game_OnInput;
+        }
+
+        private static void Game_OnInput(GameInputEventArgs args)
+        {
+            if (args.Input.StartsWith("/model"))
+            {
+                args.Process = false;
+                ObjectManager.Player.SetSkin(args.Input.Replace("/model ", string.Empty), 0);
+                return;
+            }
+
+            if (args.Input.StartsWith("/skin"))
+            {
+                args.Process = false;
+                ObjectManager.Player.SetSkin(
+                    ObjectManager.Player.BaseSkinName, Convert.ToInt32(args.Input.Replace("/skin ", string.Empty)));
+            }
+        }
+    }
+}
--- SkinHack/Properties/AssemblyInfo.cs
+++ SkinHack/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkinHax")]
+[assembly: AssemblyTitle("SkinHack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SkinHax")]
+[assembly: AssemblyProduct("SkinHack")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e4d8f91-e62c-4ae4-b51f-1837abfc6b79")]
+[assembly: Guid("48108ec4-a0e4-408f-be4c-a7ada967a642")]
 
 // Version information for an assembly consists of the following four values:
 //
